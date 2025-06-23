0f321fe31a512ab488855e05d4ece7874e012175
April Shen <ashen@ebi.ac.uk>
2025-06-23 09:05:16 +0100

    include obsoletes when getting label and synonyms from ols

--- cmat/trait_mapping/ols.py
+++ cmat/trait_mapping/ols.py
@@ -32,25 +32,25 @@ def get_label_and_synonyms_from_ols(ontology_uri: str) -> str:
     Using provided ontology URI, build an OLS URL with which to make a request to find the term label for this URI.
 
     :param ontology_uri: A URI for a term in an ontology.
-    :return: Term label for the ontology URI provided in the parameters.
+    :return: Term label for the ontology URI provided in the parameters and set of synonymes.
     """
-    url = build_ols_query(ontology_uri)
+    url = build_ols_query(ontology_uri, include_obsoletes=True)
     json_response = json_request(url)
 
     if not json_response:
-        return None, None
+        return '', set()
 
     # If the 'elements' section is missing from the response, it means that the term is not found in OLS
     if 'elements' not in json_response:
         if '/medgen/' not in url and '/omim/' not in url:
             logger.warning('OLS queried OK but did not return any results for URL {}'.format(url))
-        return None, None
+        return '', set()
 
     # Go through all terms found by the requested identifier and try to find the one where the _identifier_ and the
     # _term_ come from the same ontology (marked by a special flag). Example of such a situation would be a MONDO term
     # in the MONDO ontology. Example of a reverse situation is a MONDO term in EFO ontology (being *imported* into it
     # at some point).
-    label = None
+    label = ''
     synonyms = set()
     for term in json_response['elements']:
         if term['isDefiningOntology']:
