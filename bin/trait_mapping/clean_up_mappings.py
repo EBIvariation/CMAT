7c6b34ed19f133269b082a50d96faa94a5632a8d
April Shen <ashen@ebi.ac.uk>
2025-12-01 13:06:55 +0000

    fix script

--- bin/trait_mapping/clean_up_mappings.py
+++ bin/trait_mapping/clean_up_mappings.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import argparse
 import csv
 import os.path
@@ -22,48 +23,46 @@ def main(mappings_file):
     mappings, target_ontology = load_ontology_mapping(mappings_file)
 
     obsolete_mappings = defaultdict(list)
-    current_mappings = defaultdict(list)
     multiple_mappings = {}
-    single_current_mappings = {}
+    current_mappings = {}
 
-    # Separate obsolete mappings
-    for trait_name in mappings:
-        uri, label = mappings[trait_name]
-        if not is_current_and_in_ontology(uri, target_ontology):
-            obsolete_mappings[trait_name].append((uri, label))
-            continue
-        current_mappings[trait_name].append((uri, label))
-
-    # Separate multiple mappings
-    for trait_name, mapping_list in current_mappings.items():
-        if len(mapping_list) > 1:
-            multiple_mappings[trait_name] = mapping_list
+    for trait_name, mapping_list in mappings.items():
+        keep_mappings = []
+        # Remove obsolete mappings
+        for uri, label in mapping_list:
+            if not is_current_and_in_ontology(uri, target_ontology):
+                obsolete_mappings[trait_name].append((uri, label))
+            else:
+                keep_mappings.append((uri, label))
+        # Remove multiple mappings
+        if len(keep_mappings) > 1:
+            multiple_mappings[trait_name] = keep_mappings
         else:
-            single_current_mappings[trait_name] = mapping_list
+            current_mappings[trait_name] = keep_mappings
 
     # Output files
-    basename = os.path.basename(mappings_file)
-    with (open(f'{basename}_obsolete.csv'), 'w') as outfile:
+    filename = '.'.join(os.path.basename(mappings_file).split('.')[:-1])
+    with open(f'{filename}_obsolete.csv', 'w+') as outfile:
         writer = csv.writer(outfile, delimiter='\t')
         obsolete_rows = flatten_dict(obsolete_mappings)
         print(f'Removed {len(obsolete_rows)} obsolete mappings')
         writer.writerows(obsolete_rows)
 
-    with (open(f'{basename}_multiple.csv'), 'w') as outfile:
+    with open(f'{filename}_multiple.csv', 'w+') as outfile:
         writer = csv.writer(outfile, delimiter='\t')
         multiple_rows = flatten_dict(multiple_mappings)
         print(f'Removed {len(multiple_rows)} multiple mappings')
         writer.writerows(multiple_rows)
 
-    with (open(f'{basename}_current.csv'), 'w') as outfile:
+    with open(f'{filename}_current.csv', 'w+') as outfile:
         writer = csv.writer(outfile, delimiter='\t')
-        current_rows = flatten_dict(single_current_mappings)
+        current_rows = flatten_dict(current_mappings)
         print(f'{len(current_rows)} mappings remaining')
         writer.writerows(current_rows)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser('Clean up obsolete and multiple mappings')
     parser.add_argument('--mappings-file', required=True, help='File of latest ontology mappings to process')
     args = parser.parse_args()
     main(args.mappings_file)
