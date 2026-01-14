132395a8622e7a63b26a5fb3b11f41ac1510e10b
April Shen <ashen@ebi.ac.uk>
2026-01-14 09:15:25 +0000

    add test for replacement mapping

--- tests/trait_mapping/test_create_table.py
+++ tests/trait_mapping/test_create_table.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bin.trait_mapping.create_table_for_manual_curation import get_mapping_attributes_from_ols
+from bin.trait_mapping.create_table_for_manual_curation import get_mapping_attributes_from_ols, find_replacement_mapping
 from cmat.trait_mapping.ols import MappingSource, MatchType
 
 
@@ -15,3 +15,20 @@ def test_get_mapping_attributes():
     assert label == 'WAGR syndrome'
     assert match_type == MatchType.EXACT_MATCH_SYNONYM
     assert mapping_source == MappingSource.TARGET_CURRENT
+
+
+@pytest.mark.integration
+def test_find_replacement_mapping():
+    trait_name = 'genetic transient congenital hypothyroidism'
+    target_ontology = 'efo'
+    preferred_ontologies = ['mondo', 'hp']
+
+    # Current in EFO - no replacement term
+    assert find_replacement_mapping(trait_name, 'http://purl.obolibrary.org/obo/MONDO_0011792', target_ontology, preferred_ontologies) == ''
+
+    # Deprecated in EFO with current replacement term
+    assert find_replacement_mapping(trait_name, 'http://www.ebi.ac.uk/efo/EFO_0000665', target_ontology, preferred_ontologies) == 'http://purl.obolibrary.org/obo/MONDO_0037939|porphyria|TOKEN_MATCH_SYNONYM|EFO_CURRENT'
+
+    # Deprecated in EFO but replacement is also deprecated.
+    # The replacement itself has a replacement in Mondo but not in EFO, so no replacement is found
+    assert find_replacement_mapping(trait_name, 'http://www.orpha.net/ORDO/Orphanet_226316', target_ontology, preferred_ontologies) == ''
